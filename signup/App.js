10bdcba16e3d9397102353d70b3d6ebaa3b96669
siddhipandya <82381765+siddhipandya@users.noreply.github.com>
2021-12-07 17:52:00 +0530

    Add files via upload

---
+++ signup/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from "./components/login"
+import SignUp from './components/signup'
+export default function App() {
+  return(
+    <View> 
+      <SignUp/>
+    </View>
+  )
+}
